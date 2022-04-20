48718794fdba54d9f0fa8ad7890321ed13ab2cc3
PRANAY BHARGAV REDDY BAKARAM <89271212+Pranayreddybakaram@users.noreply.github.com>
2022-04-20 00:08:05 -0500

    Update ApplyForJobFragment.java

--- MyProject/app/src/main/java/com/example/myproject/ApplyForJobFragment.java
+++ MyProject/app/src/main/java/com/example/myproject/ApplyForJobFragment.java
@@ -2,12 +2,10 @@ package com.example.myproject;
 
 import android.app.AppComponentFactory;
 import android.os.Build;
-
 import androidx.annotation.RequiresApi;
 
 @RequiresApi(api = Build.VERSION_CODES.P)
 public class ApplyForJobFragment extends AppComponentFactory {
-
 public class ApplyForJobFragment {
 
 }
