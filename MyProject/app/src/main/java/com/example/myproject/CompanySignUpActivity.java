43c49e738b7afe748edf3004b3938f14d523f09c
akhilmike <89603470+akhilmike@users.noreply.github.com>
2022-04-19 22:21:00 -0500

    changes to  companySignupActivity

--- MyProject/app/src/main/java/com/example/myproject/CompanySignUpActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/CompanySignUpActivity.java
@@ -6,26 +6,3 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CompanySignUpActivity extends AppCompatActivity {
-
-    Button signup;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_company_sign_up);
-
-
-        signup = findViewById(R.id.signUpButton2);
-
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_company_dashboard);
-
-            }
-        });
-
-
-    }
-}
