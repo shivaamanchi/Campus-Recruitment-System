e5b16c449e0d05861ef4e00d575b1f79c5e555bc
akhilmike <89603470+akhilmike@users.noreply.github.com>
2022-04-19 22:14:51 -0500

    changes

--- MyProject/app/src/main/java/com/example/myproject/Company.java
+++ MyProject/app/src/main/java/com/example/myproject/Company.java
@@ -1,10 +1,5 @@
 package com.example.myproject;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class Company extends AppCompatActivity {
     Button login;
