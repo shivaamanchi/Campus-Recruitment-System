6655b473d9385dc96b02252af11848a70f1504f0
PRANAY BHARGAV REDDY BAKARAM <89271212+Pranayreddybakaram@users.noreply.github.com>
2022-04-19 23:19:45 -0500

    Update AdminSignUpActivity.java

--- MyProject/app/src/main/java/com/example/myproject/AdminSignUpActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/AdminSignUpActivity.java
@@ -12,6 +12,7 @@ public class AdminSignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_admin_sign_up);
 
 
