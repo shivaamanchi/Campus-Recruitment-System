3e76ff48cba1cef1f122728e4f8d72ca4b5a5885
akhilmike <89603470+akhilmike@users.noreply.github.com>
2022-04-19 22:22:29 -0500

    updated company.java

--- MyProject/app/src/main/java/com/example/myproject/Company.java
+++ MyProject/app/src/main/java/com/example/myproject/Company.java
@@ -6,3 +6,32 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+public class Company extends AppCompatActivity {
+    Button login;
+    Button signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company);
+        login = findViewById(R.id.login_button2);
+
+        signup = findViewById(R.id.new_account_button2);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_company_dashboard);
+
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_company_sign_up);
+
+            }
+        });
+
+
+    }
+}
