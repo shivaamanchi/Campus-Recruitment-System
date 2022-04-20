e987aa1a955b90aceeb6687e03492ce7411a3971
PRANAY BHARGAV REDDY BAKARAM <89271212+Pranayreddybakaram@users.noreply.github.com>
2022-04-19 23:20:39 -0500

    Update Company.java

--- MyProject/app/src/main/java/com/example/myproject/Company.java
+++ MyProject/app/src/main/java/com/example/myproject/Company.java
@@ -3,7 +3,6 @@ package com.example.myproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Company extends AppCompatActivity {
