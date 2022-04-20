133182fc69202e40b707b72e8ab7235716550659
PRANAY BHARGAV REDDY BAKARAM <89271212+Pranayreddybakaram@users.noreply.github.com>
2022-04-19 23:16:14 -0500

    Update StudentDashboardActivity.java

--- MyProject/app/src/main/java/com/example/myproject/StudentDashboardActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/StudentDashboardActivity.java
@@ -7,17 +7,14 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.myproject.AboutUsActivity;
 import com.example.myproject.MainActivity;
 import com.example.myproject.R;
@@ -30,9 +27,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import org.jetbrains.annotations.NotNull;
-
 public class StudentDashboardActivity extends AppCompatActivity {
 
     private ActivityStudentDashboardBinding binding;
