ba0a7e5218007d0835e16329e8a1f4e7c0392e16
akhilmike <89603470+akhilmike@users.noreply.github.com>
2022-04-19 22:23:45 -0500

    updated

--- MyProject/app/src/main/java/com/example/myproject/CompanySignUpActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/CompanySignUpActivity.java
@@ -1,5 +1,10 @@
 package com.example.myproject;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CompanySignUpActivity extends AppCompatActivity {
 
