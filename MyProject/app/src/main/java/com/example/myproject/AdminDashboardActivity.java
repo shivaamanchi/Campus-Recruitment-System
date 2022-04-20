67997566bf72b95f7e733340cea748f12ef8f4cb
PRANAY BHARGAV REDDY BAKARAM <89271212+Pranayreddybakaram@users.noreply.github.com>
2022-04-20 00:06:59 -0500

    Update AdminDashboardActivity.java

--- MyProject/app/src/main/java/com/example/myproject/AdminDashboardActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/AdminDashboardActivity.java
@@ -34,7 +34,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import org.jetbrains.annotations.NotNull;
 
 public class AdminDashboardActivity extends AppCompatActivity {
