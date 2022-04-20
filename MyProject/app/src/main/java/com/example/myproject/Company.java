1b5860ddbc757cc1469b567b86c6cdca155f135c
akhilmike <89603470+akhilmike@users.noreply.github.com>
2022-04-19 21:57:17 -0500

    Changes in Company file

--- MyProject/app/src/main/java/com/example/myproject/Company.java
+++ MyProject/app/src/main/java/com/example/myproject/Company.java
@@ -24,13 +24,7 @@ public class Company extends AppCompatActivity {
 
             }
         });
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_company_sign_up);
-
-            }
-        });
+        
 
 
     }
